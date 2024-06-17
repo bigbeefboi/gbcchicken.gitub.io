9b0bdd3ee49aa5c9bdc0c4be3f38a808a41919fe
Faiz Pathan <139258262+bigbeefboi@users.noreply.github.com>
2024-06-17 22:17:59 +0200

    fixed build issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,14 +10,23 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh','react', 'prettier'],
+  plugins: ['react-refresh','react'],
   rules: {
-        'react/react-in-jsx-scope': 'off', // Disable the rule requiring React in scope
+    'react/jsx-key':  0,
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+    "react/prop-types": 0,
+    "vue/multi-word-component-name": "off",
+    'react/react-in-jsx-scope': 'off', // Disable the rule requiring React in scope
     'no-unused-vars': ['error', { varsIgnorePattern: 'React' }], // Ignore unused React import
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+
+      
+
     ],
+    "no-unused-vars": 0
   },
 }
